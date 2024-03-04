22c6db5ffea8aa3e1770001efcdeee11cefafe5c
Akonwi Ngoh <akonwi@gmail.com>
2024-03-04 15:41:45 -0500

    auth guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,22 @@
 import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
 import { Module } from "@nestjs/common";
+import { ConfigModule } from "@nestjs/config";
 import { GraphQLModule } from "@nestjs/graphql";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { ChoreResolver } from "./chores/chore.resolver";
 
 @Module({
-	imports: [
-		GraphQLModule.forRoot<ApolloDriverConfig>({
-			driver: ApolloDriver,
-			autoSchemaFile: true,
-			sortSchema: true,
-			playground: process.env.NODE_ENV !== "production",
-		}),
-	],
-	controllers: [AppController],
-	providers: [AppService, ChoreResolver],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+      playground: process.env.NODE_ENV !== "production",
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService, ChoreResolver],
 })
 export class AppModule {}
--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -2,12 +2,19 @@ import {
   CanActivate,
   ExecutionContext,
   Injectable,
+  Logger,
   UnauthorizedException,
 } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { Request } from "express";
+import * as jose from "jose";
 
 @Injectable()
 export class AuthGuard implements CanActivate {
+  private readonly _logger = new Logger(AuthGuard.name);
+
+  constructor(private readonly _configService: ConfigService) {}
+
   async canActivate(context: ExecutionContext) {
     const request = context.switchToHttp().getRequest();
     const token = await this._getTokenFromHeader(request);
@@ -20,5 +27,24 @@ export class AuthGuard implements CanActivate {
   private async _getTokenFromHeader(request: Request): Promise<string> {
     const [type, token] = request.headers.authorization?.split(" ") ?? [];
     return type === "Bearer" ? token : undefined;
+  }
+
+  private async validateToken(token: string): Promise<string> {
+    const jwks = this._configService.getOrThrow("JWKS");
+
+    const result = await jose.jwtVerify(
+      token,
+      jose.createLocalJWKSet(JSON.parse(jwks)),
+      {
+        audience: this._configService.getOrThrow("AUTH0_AUDIENCE"),
+        issuer: this._configService.getOrThrow("AUTH0_ISSUER_BASE_URL"),
+      },
+    );
+    if (!result.payload.sub) {
+      this._logger.error("Invalid token");
+      throw new UnauthorizedException();
+    }
+
+    return result.payload.sub;
   }
 }
