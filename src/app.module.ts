9cf46cf7fb828613ded22d45750e313d5e9b5df8
Akonwi Ngoh <akonwi@gmail.com>
2024-03-16 14:28:45 -0400

    drop initial template stuff

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,8 +3,6 @@ import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { APP_GUARD } from "@nestjs/core";
 import { GraphQLModule } from "@nestjs/graphql";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
 import { AuthGuard } from "./auth/auth.guard";
 import { ChoreRepository } from "./chores/chore.repository";
 import { ChoreResolver } from "./chores/chore.resolver";
@@ -29,7 +27,7 @@ import { UserResolver } from "./users/user.resolver";
       playground: process.env.NODE_ENV !== "production",
     }),
   ],
-  controllers: [AppController],
+  controllers: [],
   providers: [
     PrismaService,
     AppService,
