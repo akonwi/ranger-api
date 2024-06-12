b8f7eb1c5fd6e1b60f510b997a61cb51a77c380b
Akonwi Ngoh <akonwi@gmail.com>
2024-06-11 22:24:26 -0400

    with 5 chores

--- src/assignments/assignmentStrategies.test.ts
+++ src/assignments/assignmentStrategies.test.ts
@@ -204,6 +204,190 @@ describe("Assignment Strategies", () => {
           );
         });
       });
+
+      describe("with 5 chores", () => {
+        let cleanKitchen: Chore;
+        let cleanBathroom: Chore;
+        let takeOutTrash: Chore;
+        let vacuum: Chore;
+        let laundry: Chore;
+
+        beforeAll(async () => {
+          cleanKitchen = await app.get(ChoreRepository).create({
+            creatorId: house.memberIds[0],
+            name: "Clean the kitchen",
+            description: "",
+            frequency: Frequency.WEEKLY,
+            house: {
+              connect: { id: house.id },
+            },
+          });
+          cleanBathroom = await app.get(ChoreRepository).create({
+            creatorId: house.memberIds[0],
+            name: "Clean the bathroom",
+            description: "",
+            frequency: Frequency.WEEKLY,
+            house: {
+              connect: { id: house.id },
+            },
+          });
+          takeOutTrash = await app.get(ChoreRepository).create({
+            creatorId: house.memberIds[0],
+            name: "Take out trash",
+            description: "",
+            frequency: Frequency.WEEKLY,
+            house: {
+              connect: { id: house.id },
+            },
+          });
+          vacuum = await app.get(ChoreRepository).create({
+            creatorId: house.memberIds[0],
+            name: "Vacuum",
+            description: "",
+            frequency: Frequency.WEEKLY,
+            house: {
+              connect: { id: house.id },
+            },
+          });
+          laundry = await app.get(ChoreRepository).create({
+            creatorId: house.memberIds[0],
+            name: "Laundry",
+            description: "",
+            frequency: Frequency.WEEKLY,
+            house: {
+              connect: { id: house.id },
+            },
+          });
+        });
+
+        it("alternates the chore between members", async () => {
+          const [alice, bob] = ["alice", "bob"];
+          const strategy = new RoundRobinStrategy(app.get(AssignmentService), {
+            members: [alice, bob],
+            houseId: house.id,
+            chores: [
+              cleanKitchen,
+              cleanBathroom,
+              takeOutTrash,
+              vacuum,
+              laundry,
+            ],
+          });
+
+          const week0Result = await strategy.apply(0);
+          expect(week0Result[alice]).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                choreId: cleanKitchen.id,
+                isPenalty: false,
+                week: 0,
+              }),
+              expect.objectContaining({
+                choreId: takeOutTrash.id,
+                isPenalty: false,
+                week: 0,
+              }),
+              expect.objectContaining({
+                choreId: laundry.id,
+                isPenalty: false,
+                week: 0,
+              }),
+            ]),
+          );
+          expect(week0Result[bob]).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                choreId: cleanBathroom.id,
+                isPenalty: false,
+                week: 0,
+              }),
+              expect.objectContaining({
+                choreId: vacuum.id,
+                isPenalty: false,
+                week: 0,
+              }),
+            ]),
+          );
+
+          await app
+            .get(AssignmentRepository)
+            .createMany(Object.values(week0Result).flat());
+
+          const week1Result = await strategy.apply(1);
+          expect(week1Result[alice]).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                choreId: cleanBathroom.id,
+                isPenalty: false,
+                week: 1,
+              }),
+              expect.objectContaining({
+                choreId: vacuum.id,
+                isPenalty: false,
+                week: 1,
+              }),
+            ]),
+          );
+          expect(week1Result[bob]).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                choreId: cleanKitchen.id,
+                isPenalty: false,
+                week: 1,
+              }),
+              expect.objectContaining({
+                choreId: takeOutTrash.id,
+                isPenalty: false,
+                week: 1,
+              }),
+              expect.objectContaining({
+                choreId: laundry.id,
+                isPenalty: false,
+                week: 1,
+              }),
+            ]),
+          );
+
+          await app
+            .get(AssignmentRepository)
+            .createMany(Object.values(week1Result).flat());
+
+          const week2Result = await strategy.apply(2);
+          expect(week2Result[bob]).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                choreId: cleanBathroom.id,
+                isPenalty: false,
+                week: 2,
+              }),
+              expect.objectContaining({
+                choreId: vacuum.id,
+                isPenalty: false,
+                week: 2,
+              }),
+            ]),
+          );
+          expect(week2Result[alice]).toEqual(
+            expect.arrayContaining([
+              expect.objectContaining({
+                choreId: cleanKitchen.id,
+                isPenalty: false,
+                week: 2,
+              }),
+              expect.objectContaining({
+                choreId: takeOutTrash.id,
+                isPenalty: false,
+                week: 2,
+              }),
+              expect.objectContaining({
+                choreId: laundry.id,
+                isPenalty: false,
+                week: 2,
+              }),
+            ]),
+          );
+        });
+      });
     });
   });
 });
