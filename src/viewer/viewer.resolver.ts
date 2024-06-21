b36075df0bb4af59081af684677aec8710355818
Akonwi Ngoh <2013804+akonwi@users.noreply.github.com>
2024-06-21 16:45:45 -0400

    refactor: remove deprecated resolvers (#11)

--- src/viewer/viewer.resolver.ts
+++ src/viewer/viewer.resolver.ts
@@ -1,28 +1,16 @@
-import { Query, Resolver, ResolveField, Mutation, Args } from "@nestjs/graphql";
+import { Query, Resolver, Mutation, Args } from "@nestjs/graphql";
 import { Viewer } from "./viewer.model";
 import { CurrentUser, UserContext } from "../auth/currentUser.decorator";
 import { UserService } from "../users/user.service";
-import { House } from "../houses/house.model";
 import { Maybe } from "../utils";
-import { HouseService } from "../houses/house.service";
 
 @Resolver(() => Viewer)
 export class ViewerResolver {
-  constructor(
-    private readonly _houseService: HouseService,
-    private readonly _userService: UserService,
-  ) {}
+  constructor(private readonly _userService: UserService) {}
 
   @Query(() => Viewer, { nullable: true })
   async viewer(@CurrentUser() ctx: UserContext): Promise<Maybe<Viewer>> {
     return this._userService.get(ctx.id);
-  }
-
-  @ResolveField("house", () => House, { nullable: true })
-  async getMyHouse(@CurrentUser() user: UserContext): Promise<Maybe<House>> {
-    const house = await this._houseService.getForUser(user.id);
-    if (house) return house;
-    return this._houseService.maybeConfirmInvite(user.id);
   }
 
   @Mutation(() => Boolean)
